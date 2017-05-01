60596cecc28bb80002856ebf0e74e49517ab13e0
Mark <markjconlon@gmail.com>
2017-05-01 13:23:55 -0400

    finished 1-4

--- ajax.js
+++ ajax.js
@@ -1,5 +1,25 @@
 $(document).ready(function () {
 
-  /* Your code goes here */
+  $('#runAJAX').click(function() {
+    $.ajax({
+      url: 'http://first-ajax-api.herokuapp.com/',
+      method: 'GET',
+      dataType: 'html',
+    }).done(function(data) {
+      $('body').append(data);
+    });
+  });
 
+  $('#runAJAXping').click(function() {
+    $.ajax({
+      url: 'http://first-ajax-api.herokuapp.com/pong',
+      method: 'GET',
+      // dataType: 'html',
+    }).done(function(data) {
+      $('#step3456').append(data);
+    }).fail(function(jqXHR, textStatus){
+      console.log('.ajax Failed with ' + jqXHR.status + ' ' + jqXHR.statusText);
+      $('#step3456').append("Sorry we will try harder next time.");
+    });
+  });
 });
